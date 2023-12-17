e9d611d54451485e345cefeba071b2a6998f8420
8576-09917 <132528322+8576-09917@users.noreply.github.com>
2023-12-17 14:46:30 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,1 +1,8 @@
-
+API_ID       = 21943287
+API_HASH     = "43b7ef733f4cf37770e737956fa3cec5"
+BOT_TOKEN    = "6012346500:AAGMVt_Y5e9UKbIiFm_uueP8GEmY4TMMYTU"
+SESSION      = "BQFO0_cAQ3NdEe9yOvYV695IeR1oCDJrAP4TJyaKfbiC_CggKffIc8a6nRwUGnpZ6njR6_g4OiH8N9CG3bxg2LiR4L8B8n-mfWk3o13GVjvrGr5qw826JeP9IJXubIor54JbBSgoUrAfO95YehdiyGr9ZeA-Gz7wdso6bDUvdwYA-EMsCH9vEvG_8ch0SSR02HY4bu-WtO1x6kNQkP7IOVgaKBLW05gmPb5N2OWB74_RkRRAAvg7qB4MNn3tofPheqCEIzTZgqA2muvUjbw4-dRrQLFVgGRuS18rhqJwJinYJm7NZZ89L9n7oINlripx2UpBji_q5_im9XumfiyEeGNtrY-KwwAAAABMEn1lAA "
+DATABASE_URI = "mongodb+srv://Shazambot:password1234@cluster0.frux28j.mongodb.net/?retryWrites=true&w=majority"
+LOG_CHANNEL  = -1001919010755
+ADMIN        = 1947565279
+CHANNEL      = "cinemaa_boxoffice"
